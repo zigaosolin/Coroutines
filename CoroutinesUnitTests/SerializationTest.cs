ca6f2f2db76e530051cfd243a0301af318ee8663
zigaosolin <ziga.osolin@gmail.com>
2018-07-19 22:50:12 +0200

    Managed to get iterator to serialize
    I expect too many problems with JSON serialization that this is worth
    persuing. We will try a middle way; only allow reactors to be serialized
    with special event that will finish all pending coroutines and then
    relocate object. All new starts will already be enqueued to the new
    reactor

--- CoroutinesUnitTests/SerializationTest.cs
+++ CoroutinesUnitTests/SerializationTest.cs
@@ -1,13 +1,83 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 using Xunit;
+using System.Linq;
 
 namespace Coroutines.Tests
 {
     public class SerializationTest
     {
+
+        public class MyContractResolver : DefaultContractResolver
+        {
+            
+
+            protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+            {
+                if (type == typeof(EventQueue) || type == typeof(Coroutine) ||
+                   type == typeof(EventCoroutineScheduler))
+                {
+                    var props = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                                       .Select(f => base.CreateProperty(f, memberSerialization))
+                            .ToList();
+                    props.ForEach(p => { p.Writable = true; p.Readable = true; });
+                    return props;
+                }
+                else
+                {
+                    var props = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                                    .Where(p => p.CanRead && p.CanWrite)
+                                    .Select(p => base.CreateProperty(p, memberSerialization))
+                                .Union(type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                                           .Select(f => base.CreateProperty(f, memberSerialization)))
+                                .ToList();
+                    props.ForEach(p => { p.Writable = true; p.Readable = true; });
+                    return props;
+                }
+            }
+        }
+
+        IEnumerator<IWaitObject> SimpleEnumerator()
+        {
+            int a = 0;
+            yield return null;
+            a = 1;
+        }
+
+        class EnumeratorHolder
+        {
+            public IEnumerator<IWaitObject> Enumerator;
+        }
+
+        [Fact]
+        public void SerializeIterator()
+        {
+            var enumerator = SimpleEnumerator();
+            var holder = new EnumeratorHolder()
+            {
+                Enumerator = enumerator
+            };
+            Assert.True(enumerator.MoveNext());
+
+            JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.Auto,
+                PreserveReferencesHandling = PreserveReferencesHandling.All,
+                ContractResolver = new MyContractResolver()
+            };
+
+            var data = JsonConvert.SerializeObject(holder, jsonSerializerSettings);
+
+            var newHolder = JsonConvert.DeserializeObject<EnumeratorHolder>(data, jsonSerializerSettings);
+            Assert.False(newHolder.Enumerator.MoveNext());
+
+        }
+
         public class SerializedCoroutine : Coroutine
         {
             public int Iteration { get; set; } = 0;
@@ -26,7 +96,9 @@ namespace Coroutines.Tests
             JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
             {
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                TypeNameHandling = TypeNameHandling.Auto
+                TypeNameHandling = TypeNameHandling.Auto,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ContractResolver = new MyContractResolver()
             };
 
             string data;
@@ -58,7 +130,9 @@ namespace Coroutines.Tests
             JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings()
             {
                 ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                TypeNameHandling = TypeNameHandling.Auto
+                TypeNameHandling = TypeNameHandling.Auto,
+                PreserveReferencesHandling = PreserveReferencesHandling.Objects,
+                ContractResolver = new MyContractResolver()
             };
 
             string data;
